e4192d6e37cef8684c6a92ba538efe20f246ae46
Matt Polito <matt.polito@gmail.com>
2009-08-15 12:22:43 -0500

    added aasm

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem 'binarylogic-settingslogic', :lib => 'settingslogic', :source => 'http://gems.gitub.com'
   config.gem 'haml'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http:gems.gitub.com'
+  config.gem "rubyist-aasm", :lib => "aasm"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
