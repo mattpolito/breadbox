54a0a92948a5eed1e19c8f9b67b702628e1ca52d
Matt Polito <matt.polito@gmail.com>
2009-08-17 20:45:04 -0500

    added status detect for proper css class

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,12 @@
 module DashboardHelper
+  def status_class(status)
+    case status
+    when 'draft'
+      { :class => 'draft' }
+    when 'sent'
+      { :class => 'sent' }
+    when 'paid'
+      { :class => 'paid' }
+    end
+  end
 end
