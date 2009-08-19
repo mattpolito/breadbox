6349fc94660919600452c232fcf648fe37004531
Matt Polito <matt.polito@gmail.com>
2009-08-19 09:16:13 -0500

    limit newly_created output to 10 invoices for now

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @overdue_invoices = Invoice.overdue
-    @new_invoices = Invoice.newly_created(5).order('created_at DESC')
+    @new_invoices = Invoice.newly_created(5).order('created_at DESC').limit(10)
     @estimated_total_for_year = Invoice.yearly_estimated_income
   end
 end
