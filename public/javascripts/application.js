39bc0464ceab65fab012cdd5aaefbc1f60947aca
Stephen Korecky <skorecky@gmail.com>
2009-08-19 21:28:17 -0500

    started to add to spam email link, working on client show page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function nospam(user,domain) {
+	locationstring = "mailto:" + user + "@" + domain;
+	window.location = locationstring;
+}
+
 function insert_fields(link, method, content) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + method, "g")
