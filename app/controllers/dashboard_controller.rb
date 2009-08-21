fc7924cd57b8a574b53ec2cabce23a381e4df3f6
Matt Polito <matt.polito@gmail.com>
2009-08-20 22:31:24 -0500

    made new dashboard sections work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   def index
     @overdue_invoices = Invoice.overdue
     @new_invoices = Invoice.newly_created(5).order('created_at DESC').limit(10)
+    @sent_invoices = Invoice.sent.limit(10)
+    @paid_invoices = Invoice.paid.limit(10)
     @estimated_total_for_year = Invoice.yearly_estimated_income
   end
 end
