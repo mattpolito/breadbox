8e9a9ad20ea6419007e8d750378203f2eac52d92
Matt Polito <matt.polito@gmail.com>
2009-08-20 22:21:41 -0500

    link and path fixes

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,7 @@
 class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
+  layout 'home'
   
   def new
     @user_session = UserSession.new
@@ -10,7 +11,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default account_url
+      redirect_back_or_default root_url
     else
       render :action => :new
     end
