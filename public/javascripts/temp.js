7d85234b1565f5253ce58faa20fee996ee51ff31
Stephen Korecky <skorecky@gmail.com>
2009-08-21 20:12:46 -0500

    added js to login

--- public/javascripts/temp.js
+++ public/javascripts/temp.js
@@ -2,6 +2,22 @@ jQuery.noConflict();
 (function($) { 
   $(function() {
     $(document).ready(function()  {
+      // Swap Value in input fields
+      swapValues = [];
+      $(".swap_value").each(function(i){
+        swapValues[i] = $(this).val();
+        $(this).focus(function(){
+          if ($(this).val() == swapValues[i]) {
+            $(this).val("").css({ color: "#FFF"});
+            $(this).siblings("label").css({ color: "#FFF"});
+          }
+        }).blur(function(){
+          if ($.trim($(this).val()) == "") {
+            $(this).val(swapValues[i]).css({ color: "#444"});
+            $(this).siblings("label").css({ color: "#444"});
+          }
+        });
+      });
       
     });
   });
