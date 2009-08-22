f42869efcde68e55a87bf9941935c6d82036d43e
Matt Polito <matt.polito@gmail.com>
2009-08-22 08:42:38 -0500

    removed require no user so that an admin can create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  before_filter :require_no_user, :only => [ :new, :create ]
   before_filter :require_user, :only => [ :show, :edit, :update ]
   before_filter :admin_required, :only => [ :index, :destroy ]
   
