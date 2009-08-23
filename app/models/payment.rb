805306da0577b2388c9156abe5a76a6e8343632d
Matt Polito <matt.polito@gmail.com>
2009-08-22 23:13:18 -0500

    marks invoice as paid when amount due is zero

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,14 +1,27 @@
-class Payment < ActiveRecord::Base  
+class Payment < ActiveRecord::Base
+  after_save :mark_invoice_as_paid, :if => :fulfills_amount_due?
+  
   # Associations
   belongs_to :invoice
   
+  # Validations
+  validates_presence_of :amount
+  
   # Logic
   def amount
-    (amount_in_cents / 100).to_f
+    (amount_in_cents / 100).to_f unless amount_in_cents.blank?
   end
   
   def amount=(amount)
-    self.amount_in_cents = amount.to_i * 100
+    self.amount_in_cents = amount.to_i * 100 unless amount.blank?
+  end
+  
+  def mark_invoice_as_paid
+    invoice.paid! unless invoice.paid?
+  end
+  
+  def fulfills_amount_due?
+    (invoice.amount_due <= 0) unless invoice.blank?
   end
   
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -11,9 +11,38 @@ describe Payment do
     Payment.create!(Factory.attributes_for(:payment))
   end
   
-  it "should capture payment and convert to pennies before saving" do
-    invoice = Factory(:invoice)
-    payment = invoice.payments.create!(:amount => "10.00")
-    payment.amount_in_cents.should == 1000
+  describe "invoice" do
+    it "should get marked paid if amount_due is 0" do
+      invoice = Factory(:invoice)
+      3.times { Factory(:invoice_line, :invoice => invoice, :price => "50") }
+      3.times { invoice.payments.create! :amount => "250" }
+      invoice.reload
+      invoice.should be_paid
+    end
   end
+
+  describe "amount" do
+    it "should capture payment and convert to pennies before saving" do
+      invoice = Factory(:invoice)
+      payment = invoice.payments.create!(:amount => "10.00")
+      payment.amount_in_cents.should == 1000
+    end
+    
+    it "should display money value in a float" do
+      invoice = Factory(:invoice)
+      payment = invoice.payments.create!(:amount => "10.00")
+      payment.amount.should == 10.0
+    end    
+  end
+  
+  describe "validations" do
+    it "should require amount" do
+      invoice = Factory(:invoice)
+      payment = invoice.payments.build
+      payment2 = invoice.payments.build :amount => "10.00"
+      payment.should_not be_valid
+      payment2.should be_valid
+    end
+  end
+
 end
