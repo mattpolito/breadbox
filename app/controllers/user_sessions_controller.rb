ebb5a538fdebb0d034766546007f6fb203c6a9a3
Matt Polito <matt.polito@gmail.com>
2009-08-24 20:41:06 -0500

    conditional flash message on login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -10,7 +10,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Login successful!"
+      flash[:notice] = @user_session.welcome_message
       redirect_back_or_default root_url
     else
       render :action => :new
--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,2 +1,10 @@
 class UserSession < Authlogic::Session::Base
+  def welcome_message
+    case
+    when user.login_count > 1
+      "Welcome back"
+    when user.login_count = 1
+      "Welcome, please take a look around!"
+    end
+  end
 end
