9deeb6a16bd72dd42554dd8ec9e098abb0694dfb
Matt Polito <matt.polito@gmail.com>
2009-08-27 12:47:17 -0500

    dashboard categories no longer show if now records are there

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,12 +3,12 @@ class DashboardController < ApplicationController
   
   def index
     @overdue_invoices = Invoice.overdue
-    @new_invoices = Invoice.draft.limit(10)
+    @open_invoices = Invoice.draft.limit(10)
     @sent_invoices = Invoice.sent.limit(10)
     @paid_invoices = Invoice.paid.limit(10)
     @estimated_total_for_year = Invoice.yearly_estimated_income
     @total_paid_invoices_count = Invoice.paid.size
     @total_sent_invoices_count = Invoice.sent.size
-    @total_newly_created_invoices_count = Invoice.draft.size
+    @total_open_invoices_count = Invoice.draft.size
   end
 end
