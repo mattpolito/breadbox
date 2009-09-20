045e9988c062e74ad3814e498f860b7b9cfa39e0
Matt Polito <matt.polito@gmail.com>
2009-09-19 21:53:17 -0500

    moved client avatar link/photo to client helper

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,8 @@
 module ClientsHelper
+  def client_avatar(client)
+    image_tag('client.png', :size => '75x75')
+    content_tag :label do
+      h client.first_name if client.first_name.present?
+    end
+  end
 end
