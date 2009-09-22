5b71e9e82cbec9472448326ddc6c1e8355097a35
Matt Polito <matt.polito@gmail.com>
2009-09-22 09:05:20 -0500

    added validation and specs for account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,14 @@
 class Account < ActiveRecord::Base
+  # Callbacks
+  before_save :ensure_subdomain_is_lowercase
+  
+  # Validations
+  validates_presence_of :subdomain
+  validates_uniqueness_of :subdomain
+  validates_format_of :subdomain, :with => /^([a-z]|[0-9]|-)*$/
+  
+  private
+    def ensure_subdomain_is_lowercase
+      self.subdomain = subdomain.downcase
+    end
 end
