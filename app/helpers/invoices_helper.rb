45f6eb238da0a2fd7dfc4167f2c09c6611ed42bb
Matt Polito <matt.polito@gmail.com>
2009-09-23 07:24:35 -0500

    payment notice now displaying total amount due

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -7,7 +7,7 @@ module InvoicesHelper
     content_tag :p, :class => 'notice' do
       string = "A payment in the amount of "
       string += content_tag :strong do
-        h(cents_to_dollars(invoice.total_amount))
+        h(cents_to_dollars(invoice.amount_due))
       end
       string += " #{invoice.is_or_was} due on "
       string += content_tag :strong do
