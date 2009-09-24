fccddce0a7408ef2916885579a2ba66cf18c0f6b
Matt Polito <matt.polito@gmail.com>
2009-09-24 10:56:36 -0500

    user association to organization

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   acts_as_authentic
   has_gravatar :size => 75
   
+  # Associations
+  belongs_to :organization
+  
   # Validations
   validates_presence_of :first_name
   validates_presence_of :last_name  
