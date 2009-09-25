7db9f01e2c203051db5cedb87e7dcc726340bf0f
Matt Polito <matt.polito@gmail.com>
2009-09-25 07:15:37 -0500

    normalized attributes on client and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Extensions
   acts_as_authentic
   has_gravatar :size => 75
+  normalize_attributes :first_name, :last_name, :email, :current_login_ip, :last_login_ip, :current_login_ip
   
   # Validations
   validates_presence_of :first_name
