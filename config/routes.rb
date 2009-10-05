3d39287d3be5a5fd3d74753511ad6be180af2e5a
Tukaiz <tukaiz@studioimac14.tukaiz.com>
2009-10-05 10:17:51 -0500

    fixed incorrect dashboard link

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => 'marketing', :action => 'index'
-  map.dashboard '/dashboard', :controller => 'dashboard'
+  map.dashboard '/dashboard', :controller => 'dashboard', :action => 'index'
   map.home '/home', :controller => "pages", :action => "home"
   map.login '/login', :controller => 'user_sessions', :action => 'new'
   map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
