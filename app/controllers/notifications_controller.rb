5f4502bd7319588eca35fd3ee31226bd14660986
Matt Polito <matt.polito@gmail.com>
2009-10-15 10:17:59 -0500

    notification class that allows for different types of messages being sent

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,12 @@
 class NotificationsController < ApplicationController
+  
   # /deliver/estimates/1
   # /deliver/invoices/1
+  # /deliver/reminder/1
   def deliver
-    raise params.inspect
+    Notification.deliver_message(:type => params[:type], :id => params[:id])
+    add_notice "#{params[:type].humanize} has been sent!"
+    redirect_to :back
   end
   
 end
