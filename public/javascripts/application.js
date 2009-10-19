79fe58ff35e45866da5e75a6eb3bd6c16f7d52d0
Stephen Korecky <skorecky@gmail.com>
2009-10-19 16:05:59 -0500

    adding jquery ui, re-worked sidebar with new markup, add flash to removed invoice lines

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -23,26 +23,28 @@ $(function() {
 	  if(hidden_field) {
 	    hidden_field.value = '1';
 	  }
-	  $(this).parent().parent().hide();
+	  $(this).parent().parent().effect("highlight", {}, 500, function(){
+	    $(this).fadeOut(300);
+	  });
 	  return false;
 	});
   
-	$('form a.add_child').live('click', function() {
-		var assoc = $(this).attr('data-association');
-		var content = $('#' + assoc + '_fields_template').html();
-		var regexp = new RegExp('new_' + assoc, 'g');
-		var new_id = new Date().getTime();
-		
-		$(this).parent().before(content.replace(regexp, new_id));
-		return false;
-	});
-	
-	$('form a.remove_child').live('click', function() {
-	  var hidden_field = $(this).prev('input[type=hidden]')[0];
-	  if(hidden_field) {
-	    hidden_field.value = '1';
-	  }
-	  $(this).parent('.fields').hide();
-	  return false;
-	});
+  // $('form a.add_child').live('click', function() {
+  //  var assoc = $(this).attr('data-association');
+  //  var content = $('#' + assoc + '_fields_template').html();
+  //  var regexp = new RegExp('new_' + assoc, 'g');
+  //  var new_id = new Date().getTime();
+  //  
+  //  $(this).parent().before(content.replace(regexp, new_id));
+  //  return false;
+  // });
+  // 
+  // $('form a.remove_child').live('click', function() {
+  //   var hidden_field = $(this).prev('input[type=hidden]')[0];
+  //   if(hidden_field) {
+  //     hidden_field.value = '1';
+  //   }
+  //   $(this).parent('.fields').hide();
+  //   return false;
+  // });
 });
