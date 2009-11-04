0234ff15d812f1d059677a038e36f278eb306690
Matt Polito <matt.polito@gmail.com>
2009-11-04 06:36:23 -0600

    more missed gemcutter gem config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,5 +30,5 @@ config.action_mailer.delivery_method = :test
 # Test only gems
 config.gem 'rspec', :lib => false
 config.gem 'rspec-rails', :lib => false
-config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
-config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
+config.gem 'shoulda', :source => 'http://gemcutter.org'
+config.gem 'factory_girl', :source => 'http://gemcutter.org'
