b3bbc58325d84e532a3600095b9a4262f40b279a
Matt Polito <matt.polito@gmail.com>
2009-12-28 15:55:08 -0600

    setting up basic email invoice notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,28 +1,31 @@
 class Notification < ActionMailer::Base
   def self.deliver_message(options)
     send("send_#{options[:type]}", options)
-    
-    # case options[:type]
-    # when 'invoice'
-    #   send_invoice(options)
-    # end
   end
   
   def self.send_invoice(options)
     invoice = Invoice.find(options[:id])
+    deliver_invoice(invoice)
     invoice.sent!
-    # deliver_invoice(invoice)
   end
   
   def self.send_reminder(options)
     invoice = Invoice.find(options[:id])
-    # send("deliver_#{options[:type]}_reminder", invoice)
+    send("deliver_#{options[:type]}_reminder", invoice)
   end
   
   def invoice(invoice)
     recipients  invoice.client.email
-    from        invoice.organization.email
+    from        Settings.noreply_address
     subject     "[#{invoice.organization.name}] Invoice for requested work"
+    sent_on     Time.now
+    body        :invoice => invoice
+  end
+  
+  def invoice_reminder(invoice)
+    recipients  invoice.client.email
+    from        Settings.noreply_address
+    subject     "[#{invoice.organization.name}] Invoice ##{invoice.number} payment reminder"
     sent_on     Time.now
     body        :invoice => invoice
   end
