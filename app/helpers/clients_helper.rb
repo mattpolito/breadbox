99ce3ce82992cba636f7f6a487afa46ee3cc6afc
Matt Polito <matt.polito@gmail.com>
2009-12-29 10:15:56 -0600

    added alt tag to client avatar

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,6 +1,6 @@
 module ClientsHelper
   def client_avatar(client)
-    html = image_tag(client.gravatar_url)
+    html = image_tag(client.gravatar_url, :alt => 'loading')
     html += content_tag :label do
       h client.first_name unless client.first_name.blank?
     end
