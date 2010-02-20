b8e2023c016e7648f5dec5f8579c2614a8a26c9a
Stephen Korecky <skorecky@gmail.com>
2010-02-20 09:51:12 -0600

    updated jquery to 1.4.2, fixed login highlight issues, thanks adam

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,27 +1,18 @@
-$(document).ready(function()  {
-
-  // User Login value set
-  $("#user_details input").each(function(){
-    if($(this).attr("value") == ""){
-      $(this).attr("value", $(this).siblings("label").text());
-      $(this).addClass("swap_value");
-    }
+$(document).ready(function(){
+  $('.swap_value').each(function(index){
+    fieldHighlight(this);
   });
-
-  // Swap Value
-  swapValues = [];
-  $(".swap_value").each(function(i){
-    swapValues[i] = $(this).val();
-    $(this).focus(function(){
-      if ($(this).val() == swapValues[i]) {
-        $(this).val("").css({ color: "#FFF"});
-        $(this).siblings("label").css({ color: "#FFF"});
-      }
-    }).blur(function(){
-      if ($.trim($(this).val()) == "") {
-        $(this).val(swapValues[i]).css({ color: "#444"});
-        $(this).siblings("label").css({ color: "#444"});
-      }
-    });
+	$(".swap_value").keyup(function(){
+    fieldHighlight(this);
   });
 });
+
+function fieldHighlight(element){
+	if($(element).val() != ""){
+		$(element).css({ color:"#FFF"});
+		$(element).siblings("label").css({ color:"#FFF"});
+	} else {
+		$(element).css({ color:"#444"});
+		$(element).siblings("label").css({ color:"#444"});
+	};
+}
