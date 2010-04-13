6c90f65e118d7432cf0f777dc8eddcaa5ab5927e
Matt Polito <matt.polito@gmail.com>
2010-04-13 13:45:52 -0500

    added smtp settings in development that allow for mocksmtp app capture of email

--- config/environment.rb
+++ config/environment.rb
@@ -62,16 +62,5 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
   :regular => "%B %d, %Y"
 )
 
-# Mailer settings
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-  # :address              => Settings.mail_smtp_address,
-  # :port                 => Settings.mail_smtp_port,
-  # :domain               => Settings.mail_domain,
-  # :user_name            => Settings.mail_user_name,
-  # :password             => Settings.mail_password, 
-  # :authentication       => Settings.mail_authentication_type
-# }
-
 # Google Analytics gem settings
 # Rubaidh::GoogleAnalytics.tracker_id = Settings.google_analytics_id
